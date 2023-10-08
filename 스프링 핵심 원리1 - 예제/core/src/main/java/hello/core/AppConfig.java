849483510900f66e4189753ade3b0e5909f67c82
suyong choi <dpfprtus@gmail.com>
2023-10-08 22:57:36 +0900

    refactor: 역할이 잘 들어나도록 appConfig 리팩토링

--- 스프링 핵심 원리1 - 예제/core/src/main/java/hello/core/AppConfig.java
+++ 스프링 핵심 원리1 - 예제/core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,12 +11,23 @@ import hello.core.order.OrderServiceImpl;
 
 //실제 동작에 필요한 구현 객체를 생성 -> DIP 준수
 public class AppConfig {
+
+    //메소드 이름을 보면 역할이 잘 들어난다.
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
