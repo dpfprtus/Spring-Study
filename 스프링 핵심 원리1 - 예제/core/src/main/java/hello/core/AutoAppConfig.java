cd204e7f71f90dddff8211d329513465895eb78c
suyong choi <dpfprtus@gmail.com>
2023-10-13 10:38:25 +0900

    feat: Bean 자동 등록 vs 수동등록 -> 수동등록이 오버라이딩함(우선순위가 더 높다)

--- 스프링 핵심 원리1 - 예제/core/src/main/java/hello/core/AutoAppConfig.java
+++ 스프링 핵심 원리1 - 예제/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,7 @@
 package hello.core;
 
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -7,8 +9,11 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
         basePackages = "hello.core.member",
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class) //AppConfig 제외
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //AppConfig 제외
 ) //@Component가 붙은 애들을 bean 다 등록해준다.
 public class AutoAppConfig {
-
+    @Bean(name = "memoryMemberRepository")
+    MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
