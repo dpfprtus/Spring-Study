57a358aa16f25014c96281dadf3aeb4c95785f6d
suyong choi <dpfprtus@gmail.com>
2023-10-14 16:03:29 +0900

    feat: Autowired 옵션 테스트

---
+++ 스프링 핵심 원리1 - 예제/core/src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,39 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+        //Bean에 등록되어 있지 않으면 아예 메서드 호출이 되지 않는다.
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+
+}
