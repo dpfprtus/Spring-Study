dda520b8c93c682c944a1c1c68fba60221cc87ec
suyong choi <dpfprtus@gmail.com>
2023-11-14 22:17:13 +0900

    feat: Entity 설계

---
+++ 실전! 스프링부트와 JPA활용1/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    //JPA 스펙 상 생성, 값 타입은 @Setter로 변경불가하게 설계해야한다.
+    protected Address() {
+
+    }
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
