6423ccbfd951c0f27c36fa97648b84993203f87a
suyong choi <dpfprtus@gmail.com>
2023-11-16 14:36:58 +0900

    feat: item 도메인에 비즈니스 로직 추가

--- 실전! 스프링부트와 JPA활용1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 실전! 스프링부트와 JPA활용1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,8 +24,23 @@ Item {
 
     private String name;
     private int price;
-    private int stackQuantity;
+    private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    //재고 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    //재고 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more sotck");
+        }
+        this.stockQuantity = restStock;
+    }
 }
