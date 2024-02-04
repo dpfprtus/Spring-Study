069c36b31e7a15cf1c7dbc7a6d94d524eaa5ff54
dpfprtus <dpfprtus@gmail.com>
2024-02-04 19:00:14 +0900

    feat: api 폴더

---
+++ 실전! 스프링부트와 JPA활용2/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+
+}
