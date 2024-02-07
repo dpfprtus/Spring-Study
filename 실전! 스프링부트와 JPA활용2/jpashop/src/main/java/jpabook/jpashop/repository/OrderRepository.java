d330b53adcf5adae81d840e5ec6db40a8390ff21
suyong choi <dpfprtus@gmail.com>
2024-02-07 15:15:12 +0900

    feat : OneToMany 조회

--- 실전! 스프링부트와 JPA활용2/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ 실전! 스프링부트와 JPA활용2/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -14,6 +14,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderRepository {
 
+    /**
+     * <쿼리 방식 선택 권장 순서>
+     * 1. 우선 엔티티를 DTO로 변환하는 방법을 선택.
+     * 2. 필요하면 페치 조인으로 성능 최적화 -> 대부분 성능 이슈 해결
+     * 3. 그래도 안되면 DTO로 직접 조회
+     * 4. 최후의 방법은 JPA가 제공하는 네이티브SQL이나 스프링 JDBC Template을 사용해 SQL 직접 사용.
+     * **/
+
     private final EntityManager em;
 
     public void save(Order order) {
