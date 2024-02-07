ae0b81d51d84ce7827baada2fde14e416b349401
suyong choi <dpfprtus@gmail.com>
2024-02-07 15:00:23 +0900

    feat : 조회전용으로 화면에서 사용하는 패키지

--- 실전! 스프링부트와 JPA활용2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ 실전! 스프링부트와 JPA활용2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,7 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
-import jpabook.jpashop.repository.SimpleOrderQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
+import jpabook.jpashop.repository.order.simplequery.SimpleOrderQueryDto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +27,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
-
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
     @GetMapping("/api/v1/simple-orders")
     public List<Order> orderV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
@@ -57,7 +58,7 @@ public class OrderSimpleApiController {
     }
     @GetMapping("/api/v4/simple-orders")
     public List<SimpleOrderQueryDto> ordersV4(){
-        return orderRepository.findOrderDtos();
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
     @Data
