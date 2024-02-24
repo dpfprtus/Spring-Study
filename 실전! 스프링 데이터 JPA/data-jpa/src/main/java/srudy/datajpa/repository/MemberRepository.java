646fd1dabe71ade1f0c6a6661c63f9c19cca1d02
suyong choi <dpfprtus@gmail.com>
2024-02-25 01:04:57 +0900

    메서드위에 @Query로 JPQL 작성 => 로딩 시점에 오타 감지 가능

--- 실전! 스프링 데이터 JPA/data-jpa/src/main/java/srudy/datajpa/repository/MemberRepository.java
+++ 실전! 스프링 데이터 JPA/data-jpa/src/main/java/srudy/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package srudy.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import srudy.datajpa.entity.Member;
 
 import java.util.List;
@@ -9,4 +11,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
+    List<Member> findByUsername(String username);
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
