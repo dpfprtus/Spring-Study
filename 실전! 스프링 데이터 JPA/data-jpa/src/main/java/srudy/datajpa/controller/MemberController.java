8e87977762e9fe7045109149c25a184f2e3f83b8
suyong choi <dpfprtus@gmail.com>
2024-02-26 11:33:46 +0900

    도메인 클래스 컨버터 => 조회용으로만 사용!

---
+++ 실전! 스프링 데이터 JPA/data-jpa/src/main/java/srudy/datajpa/controller/MemberController.java
@@ -0,0 +1,34 @@
+package srudy.datajpa.controller;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import srudy.datajpa.entity.Member;
+import srudy.datajpa.repository.MemberRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    //도메인 클래스 컨버터 => 조회용으로만 사용해야 한다.
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
