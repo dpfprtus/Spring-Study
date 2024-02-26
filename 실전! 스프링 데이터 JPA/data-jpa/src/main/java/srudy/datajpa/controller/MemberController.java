3ca6663fe1e2877c7bc90f62562440eb6a8f4d7b
suyong choi <dpfprtus@gmail.com>
2024-02-26 11:56:19 +0900

    Web Page

--- 실전! 스프링 데이터 JPA/data-jpa/src/main/java/srudy/datajpa/controller/MemberController.java
+++ 실전! 스프링 데이터 JPA/data-jpa/src/main/java/srudy/datajpa/controller/MemberController.java
@@ -2,9 +2,13 @@ package srudy.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import srudy.datajpa.dto.MemberDto;
 import srudy.datajpa.entity.Member;
 import srudy.datajpa.repository.MemberRepository;
 
@@ -26,9 +30,16 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5,sort="username") Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+    }
 
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
